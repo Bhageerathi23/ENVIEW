a615d736075977973ed95f4d8be6271af05ea6b2
Bhageerathi23 <153007448+Bhageerathi23@users.noreply.github.com>
2023-12-19 19:52:23 +0530

    Create send_event.sh

---
+++ send_event.sh
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type: application/json" -d '{"timestamp": "2023-05-24T05:55:00+00:00", "is_driving_safe": false, "vehicle_id": 1234, "location_type": "highway"}' http://127.0.0.1:5000/event
